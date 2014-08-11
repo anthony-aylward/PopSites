135199ea4cbef0ccb7d280a1a929bd22281122e0
anthony-aylward <aaylward@eng.ucsd.edu>
2014-08-11 00:18:53 -0700

    Update pop_sites.sh

--- pop_sites.sh
+++ pop_sites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Identify population-specific SNP sites from an input set of sites.
-python3 ~/aaylward/PopSites/pop_sites.py \
+python ~/aaylward/PopSites/pop_sites.py \
 --buff 500 \
 --fdr 0.05 \
 --inpath ~/aaylward/Data/test.txt \
