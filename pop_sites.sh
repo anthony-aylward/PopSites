a371c8dc7872e056dec076431380d0282ececb18
anthony-aylward <aaylward@eng.ucsd.edu>
2014-08-11 01:33:10 -0700

    Update pop_sites.sh

--- pop_sites.sh
+++ pop_sites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-/opt/python/bin/python2.7 ~/PopSites/pop_sites.py \
+export PATH=/opt/python/bin:$PATH
+export PYTHONPATH=/home/jpg/opt/python/lib/python2.7/site-packages:$PYTHONPATH
+python ~/PopSites/pop_sites.py \
 --buff 500 \
 --fdr 0.05 \
 --inpath ~/Data/test.txt \
