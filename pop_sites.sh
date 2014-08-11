b527fe0c748c27298c69c7524917143d3517cfa0
Anthony Aylward <aaylward@eng.ucsd.edu>
2014-08-10 23:44:09 -0700

    Updated paths

--- pop_sites.sh
+++ pop_sites.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Identify population-specific SNP sites from an input set of sites.
-python3 ~/PopSites/pop_sites.py \
+python3 ~/aaylward/PopSites/pop_sites.py \
 --buff 1000 \
 --fdr 0.05 \
---gtf ~/GTF/hg19_genes.gtf \
---inpath ~/Data/HPG_genes_with_sites_after_constraint9.txt \
+--gtf ~/aaylward/GTF/hg19_genes.gtf \
+--inpath ~/aaylward/Data/HPG_genes_with_sites_after_constraint9.txt \
 --pop CEU \
 --rep 100
 exit
